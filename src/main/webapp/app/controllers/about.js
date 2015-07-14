7ced78883ecf07f679bd15fed414de684cf180b0
Sylvain Jermini <sylvain.jermini@syjer.com>
2015-07-14 20:58:53 +0200

    ISSUTRAC-59 fix styling as desired

--- src/main/webapp/app/controllers/about.js
+++ src/main/webapp/app/controllers/about.js
@@ -5,12 +5,14 @@
 	var module = angular.module('lavagna.controllers');
 
 	module.controller('AboutLicenseCtrl', function ($scope, $http) {
+		$scope.showLicense = true;
 		$http.get('about/LICENSE-GPLv3.txt').success(function(res) {
 			$scope.license=res;
 		});
 	});
 	
 	module.controller('AboutThirdPartyCtrl', function ($scope, $http) {
+		$scope.showLicense = false;
 		$http.get('about/THIRD-PARTY.txt').success(function(res) {
 			$scope.thirdParty=res;
 		});
