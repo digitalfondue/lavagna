e2c91e013cb71fa2dff38fb1cf846fcdeaf47876
Sylvain Jermini <sylvain.jermini@syjer.com>
2015-07-14 21:32:33 +0200

    handle the uploaded file in a more user friendly way

--- src/main/java/io/lavagna/web/api/CardDataController.java
+++ src/main/java/io/lavagna/web/api/CardDataController.java
@@ -400,13 +400,12 @@ public class CardDataController {
 
 	// TODO: fix exception handling
 	@ExpectPermission(Permission.READ)
-	@RequestMapping(value = "/api/card-data/file/{fileId}", method = RequestMethod.GET)
+	@RequestMapping(value = "/api/card-data/file/{fileId}/{ignore:.+}", method = RequestMethod.GET)
 	public void getFile(@PathVariable("fileId") int fileId, HttpServletResponse response) {
 		FileDataLight fileData = cardDataRepository.getUndeletedFileByCardDataId(fileId);
-		response.addHeader("Content-Disposition", "attachment;filename=\"" + fileData.getName() + "\"");
 		try (OutputStream out = response.getOutputStream()) {
-			cardDataRepository.outputFileContent(fileData.getDigest(), out);
 			response.setContentType(fileData.getContentType());
+			cardDataRepository.outputFileContent(fileData.getDigest(), out);
 		} catch (IOException e) {
 			LOG.error("error getting file", e);
 			response.setStatus(500);
