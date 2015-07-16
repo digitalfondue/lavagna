0a3d09dd938989746fe804d0de2bfac71308703e
Simone Grignola <sito@grignola.ch>
2015-07-16 23:07:46 +0200

    Uniformed the icon position to access the project's settings

--- src/main/webapp/app/directives/lvg-navigation.js
+++ src/main/webapp/app/directives/lvg-navigation.js
@@ -8,33 +8,26 @@
 		return {
 			restrict: 'E',
 			template: '<a ng-href="#"><span data-translate>partials.home.title</span></a><i class="fa fa-angle-right"></i>' +
-			'{{project.shortName}}<span class="separator">-</span>{{project.name}}'
-			
+			'{{project.shortName}}<span class="separator">-</span>{{project.name}}<a data-lvg-has-permission="PROJECT_ADMINISTRATION" class="project-settings-shortcut" ng-href="#/{{project.shortName}}/manage/"><i class="fa fa-cog"></i></a>'
 		}
-		
 	});
-	
+
 	directives.directive('lvgBoardNavigation', function () {
 		return {
 			restrict: 'E',
 			template: '<a ng-href="#"><span data-translate>partials.home.title</span></a><i class="fa fa-angle-right"></i>' +
 			'<a ng-href="#/{{project.shortName}}/">{{project.shortName}}</a> <a data-lvg-has-permission="PROJECT_ADMINISTRATION" class="project-settings-shortcut" ng-href="#/{{project.shortName}}/manage/"><i class="fa fa-cog"></i></a><i class="fa fa-angle-right"></i>' +
 			'{{board.shortName}}<span class="separator">-</span>{{board.name}}'
-			
 		}
-		
 	});
-	
+
 	directives.directive('lvgProjectSettingsNavigation', function () {
 		return {
 			restrict: 'E',
 			template: '<a ng-href="#"><span data-translate>partials.home.title</span></a><i class="fa fa-angle-right"></i>' +
 			'<a ng-href="#/{{project.shortName}}/">{{project.shortName}}</a><i class="fa fa-angle-right"></i>' +
 			'<span data-translate>partials.project.fragments.nabvar.admin</span>'
-			
 		}
-		
 	});
-	
-	
+
 })();
