cf9004c32f8f8d32099f48ab5c0c2a5e1be071f6
Sylvain Jermini <sylvain.jermini@syjer.com>
2015-07-26 19:32:21 +0200

    WORK-303 fix permission check

--- src/main/java/io/lavagna/web/api/ProjectController.java
+++ src/main/java/io/lavagna/web/api/ProjectController.java
@@ -78,7 +78,7 @@ public class ProjectController {
 		return projectService.findAllForUserWithPermissionInProject(user);
 	}
 
-	@ExpectPermission(Permission.PROJECT_ADMINISTRATION)
+	@ExpectPermission(Permission.ADMINISTRATION)
 	@RequestMapping(value = "/api/project", method = RequestMethod.POST)
 	public void create(@RequestBody CreateRequest project) {
 		checkShortName(project.getShortName());
@@ -86,13 +86,13 @@ public class ProjectController {
 		eventEmitter.emitCreateProject(project.getShortName());
 	}
 
-	@ExpectPermission(Permission.PROJECT_ADMINISTRATION)
+	@ExpectPermission(Permission.ADMINISTRATION)
 	@RequestMapping(value = "/api/suggest-project-short-name", method = RequestMethod.GET)
 	public Suggestion suggestProjectShortName(@RequestParam("name") String name) {
 		return new Suggestion(ShortNameGenerator.generateShortNameFrom(name));
 	}
 
-	@ExpectPermission(Permission.PROJECT_ADMINISTRATION)
+	@ExpectPermission(Permission.ADMINISTRATION)
 	@RequestMapping(value = "/api/check-project-short-name", method = RequestMethod.GET)
 	public boolean checkProjectShortName(@RequestParam("name") String name) {
 		return ShortNameGenerator.isShortNameValid(name) && !projectService.existsWithShortName(name);
