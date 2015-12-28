e2aa0c38951071fc594ddcb88fa8bc308d54e535
Sylvain Jermini <sylvain.jermini@syjer.com>
2015-12-28 17:21:56 +0100

    fix: board name is now visible in board view

--- src/main/webapp/app/directives/lvg-navigation.js
+++ src/main/webapp/app/directives/lvg-navigation.js
@@ -16,7 +16,7 @@
 		return {
 			restrict: 'E',
 			template: '<a data-ui-sref="home"><span data-translate>partials.home.title</span></a><i class="fa fa-angle-right"></i>' +
-			'<a data-ui-sref="project.boards({projectName: boardCtrlResolver.project.shortName})">{{::boardCtrlResolver.project.shortName}}</a> <a data-lvg-has-permission="PROJECT_ADMINISTRATION" class="project-settings-shortcut" data-ui-sref="ProjectManage.project({projectName: boardCtrlResolver.project.shortName})" title="{{\'partials.project.fragments.nabvar.projectAdmin\' | translate}}"><i class="fa fa-cog"></i></a><i class="fa fa-angle-right"></i>' +
+			'<a data-ui-sref="project.boards({projectName: boardCtrl.project.shortName})">{{::boardCtrl.project.shortName}}</a> <a data-lvg-has-permission="PROJECT_ADMINISTRATION" class="project-settings-shortcut" data-ui-sref="ProjectManage.project({projectName: boardCtrl.project.shortName})" title="{{\'partials.project.fragments.nabvar.projectAdmin\' | translate}}"><i class="fa fa-cog"></i></a><i class="fa fa-angle-right"></i>' +
 			'{{::boardCtrl.board.shortName}}<span class="separator">-</span>{{boardCtrl.board.name}}'
 		}
 	});
