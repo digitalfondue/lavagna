031de13bf90b7ee99cc9e313d01672c5aed90b73
Sylvain Jermini <sylvain.jermini@syjer.com>
2015-12-28 13:36:04 +0100

    hack that enable a simplifacion in the title service

--- src/main/webapp/app/services/title.js
+++ src/main/webapp/app/services/title.js
@@ -4,68 +4,15 @@
 
 	var services = angular.module('lavagna.services');
 
-	services.factory('Title', function ($translate, $rootScope, $document) {
-        var currentTitle = null;
+	services.factory('Title', function ($translate, $document, $timeout) {
 
-        var canSetTitle = false;
-        var forceEmit = true;
-
-        var emit = function() {
-            var documentTitle = angular.element(window.document)[0].title;
-            if(currentTitle === documentTitle && !forceEmit) {
-                forceEmit = true;
-                canSetTitle = true;
-            } else {
-                angular.element(window.document)[0].title = currentTitle;
-            }
-        };
-
-        // put current title in the cache
-        $rootScope.$on('$locationChangeStart', function(e, n, o) {
-            //console.log('location start change, new=%s, old=%s', n, o);
-            if(n === o) { // first load yo
-                canSetTitle = true;
-            } else {
-                forceEmit = false; // not the first load anymore, stop force emit
-            }
-        });
-
-        // get the current title from the cache
-        $rootScope.$on('$locationChangeSuccess', function(e, n, o) {
-            //console.log('location success change, new=%s, old=%s', n, o);
-            if(n !== o) { // emit title when the url is changed
-                canSetTitle = false;
-                emit();
-            }
-        });
-
-        $rootScope.$on('$stateChangeStart', function(e, from, fromp, to, top) {
-            //console.log('state start change, from=%s to=%s', JSON.stringify(from), JSON.stringify(to));
-            if(to.name !== "") {
-                forceEmit = false; // first state change after first load
-            }
-        });
-
-        $rootScope.$on('$stateChangeSuccess', function(e, from, fromp, to, top) {
-            //console.log('state success change, from=%s to=%s', JSON.stringify(from), JSON.stringify(to));
-            if(to.name !== "") {
-                canSetTitle = true;
-            }
-        });
-
+		// based on stackoverflow.com/questions/23813599/set-page-title-using-ui-router
+		// yep, it's a hack.
         return {
             set: function(_title, options) {
-                if(canSetTitle) {
-                    $translate(_title, options).then(function(result) {
-                        //prevent set the same title
-                        if(currentTitle !== result) {
-                            currentTitle = result;
-                        }
-                        if(forceEmit) {
-                            emit();
-                        }
-                    });
-                }
+            	$timeout(function() {
+            		angular.element($document)[0].title = $translate.instant(_title, options);
+            	});
             }
         }
 	});
