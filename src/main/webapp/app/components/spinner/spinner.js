9b599c7398549bfbe624788a3c6027e1fda22bda
Sylvain Jermini <sylvain.jermini@syjer.com>
2015-12-31 15:58:45 +0100

    transform lvg-spinner directive in a component

---
+++ src/main/webapp/app/components/spinner/spinner.js
@@ -0,0 +1,10 @@
+(function() {
+
+    'use strict';
+
+    var components = angular.module('lavagna.components');
+    
+    components.component('lvgSpinner', {
+    	template: '<div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div>'
+    });
+})();
