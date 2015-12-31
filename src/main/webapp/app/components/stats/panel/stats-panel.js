e68f00d8918c4d6520e735a74f79e25941b18ced
Sylvain Jermini <sylvain.jermini@syjer.com>
2015-12-31 20:12:21 +0100

    refactor: componentize stats-panel

---
+++ src/main/webapp/app/components/stats/panel/stats-panel.js
@@ -0,0 +1,45 @@
+(function() {
+
+	'use strict';
+
+	angular.module('lavagna.components').component('lvgStatsPanel', {
+		templateUrl : 'app/components/stats/panel/stats-panel.html',
+		bindings : {
+			item: '=',
+			stats: '='
+		},
+		controller : function($scope, $filter) {
+			
+			var ctrl = this;
+
+			function createNormalizedData(stats) {
+				var normalizedStats = [];
+				normalizedStats.push({value: stats.openTaskCount, color: $filter('color')(stats.openTaskColor).color});
+				normalizedStats.push({value: stats.closedTaskCount, color: $filter('color')(stats.closedTaskColor).color});
+				normalizedStats.push({value: stats.backlogTaskCount, color: $filter('color')(stats.backlogTaskColor).color});
+				normalizedStats.push({value: stats.deferredTaskCount, color: $filter('color')(stats.deferredTaskColor).color});
+				return normalizedStats;
+			};
+
+			function hasTasks(stats) {
+				var totalTasks = parseInt(stats.openTaskCount) +
+					parseInt(stats.closedTaskCount) +
+					parseInt(stats.backlogTaskCount) +
+					parseInt(stats.deferredTaskCount);
+				return totalTasks > 0;
+			};
+			
+			this.chartOptions = {animation : false};
+
+			$scope.$watch('lvgStatsPanel.stats', function (stats) {
+				if (stats === undefined) {
+					return;
+				}
+				ctrl.statistics = stats;
+				if (hasTasks(stats)) {
+					ctrl.chartData = createNormalizedData(stats);
+				}
+			});
+		}
+	});
+})();
--- src/main/webapp/app/directives/lvg-stats-panel.js
+++ src/main/webapp/app/directives/lvg-stats-panel.js
@@ -4,47 +4,6 @@
 
 	var directives = angular.module('lavagna.directives');
 
-	directives.directive('lvgStatsPanel', function (Project, $filter) {
-		return {
-			templateUrl: 'partials/fragments/stats-panel.html',
-			restrict: 'E',
-			scope: {
-				item: '=',
-				stats: '='
-			},
-			link: function ($scope) {
-				$scope.chartOptions = {animation : false};
-
-				var createNormalizedData = function (stats) {
-					var normalizedStats = [];
-					normalizedStats.push({value: stats.openTaskCount, color: $filter('color')(stats.openTaskColor).color});
-					normalizedStats.push({value: stats.closedTaskCount, color: $filter('color')(stats.closedTaskColor).color});
-					normalizedStats.push({value: stats.backlogTaskCount, color: $filter('color')(stats.backlogTaskColor).color});
-					normalizedStats.push({value: stats.deferredTaskCount, color: $filter('color')(stats.deferredTaskColor).color});
-					return normalizedStats;
-				};
-
-				var hasTasks = function (stats) {
-					var totalTasks = parseInt(stats.openTaskCount) +
-						parseInt(stats.closedTaskCount) +
-						parseInt(stats.backlogTaskCount) +
-						parseInt(stats.deferredTaskCount);
-					return totalTasks > 0;
-				};
-
-				$scope.$watch('stats', function (stats) {
-					if (stats === undefined) {
-						return;
-					}
-					$scope.statistics = stats;
-					if (hasTasks(stats)) {
-						$scope.chartData = createNormalizedData(stats);
-					}
-				});
-			}
-		}
-	});
-
 	directives.directive('lvgBoardPanel', function (Board) {
 		return {
 			templateUrl: 'partials/fragments/stats-panel-board.html',
