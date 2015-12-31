3c0a95a224cde6e1912886ddbf361fadd6c9c6f1
Sylvain Jermini <sylvain.jermini@syjer.com>
2015-12-31 16:40:11 +0100

    componentize notification

---
+++ src/main/webapp/app/components/notification/notification.js
@@ -0,0 +1,13 @@
+(function() {
+
+    'use strict';
+
+    var components = angular.module('lavagna.components');
+    
+    components.component('lvgNotification', {
+    	templateUrl: 'app/components/notification/notification.html',
+    	controller: function(Notification) {
+			this.notifications = Notification.getNotifications();
+		}
+    });
+})();
--- src/main/webapp/app/services/notification.js
+++ src/main/webapp/app/services/notification.js
@@ -6,37 +6,40 @@
 
 	services.factory('Notification', function ($rootScope, $timeout) {
 
-		$rootScope.notifications = [];
+		var notifications = [];
 
 		$rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
-			$rootScope.notifications.length = 0; //empty
+			notifications.length = 0; //empty
 		});
 
 		return {
 			addNotification: function (_type, _data, _dismissable, _undo, _undoFn) {
-				$rootScope.notifications.push({event: _data, type: _type, dismissable: _dismissable, undo: _undo, undoFn : _undoFn, acknowledge : function() {
-						var index = $rootScope.notifications.indexOf(this);
-						$rootScope.notifications.splice(index, 1);
+				notifications.push({event: _data, type: _type, dismissable: _dismissable, undo: _undo, undoFn : _undoFn, acknowledge : function() {
+						var index = notifications.indexOf(this);
+						notifications.splice(index, 1);
 					}
 				});
 			},
 			addAutoAckNotification: function (_type, _data, _undo) {
 
 				var notification = {event: _data, type: _type, undo: _undo, acknowledge : function() {
-						var index = $rootScope.notifications.indexOf(this);
-						$rootScope.notifications.splice(index, 1);
+						var index = notifications.indexOf(this);
+						notifications.splice(index, 1);
 					}
 				};
 
 				$rootScope.notifications.push(notification);
 				$timeout(function () {
-					var index = $rootScope.notifications.indexOf(notification);
-					$rootScope.notifications.splice(index, 1);
+					var index = notifications.indexOf(notification);
+					notifications.splice(index, 1);
 				}, 5000);
 			},
 			acknowledgeNotification: function (notification) {
-				var index = $rootScope.notifications.indexOf(notification);
-				$rootScope.notifications.splice(index, 1);
+				var index = notifications.indexOf(notification);
+				notifications.splice(index, 1);
+			},
+			getNotifications : function () {
+				return notifications;
 			}
 		}
 	});
