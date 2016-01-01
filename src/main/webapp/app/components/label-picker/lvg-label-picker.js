4d0a242fb150f48541bb5894222f319c98da5238
Sylvain Jermini <sylvain.jermini@syjer.com>
2016-01-01 17:46:49 +0100

    WORK-312 componentize label picker directive

---
+++ src/main/webapp/app/components/label-picker/lvg-label-picker.js
@@ -0,0 +1,33 @@
+(function () {
+
+	'use strict';
+
+
+	angular.module('lavagna.components').component('lvgLabelPicker', {
+		templateUrl: 'app/components/label-picker/label-picker.html',
+		bindings: {
+			model: '=ngModel',
+			label: '=',
+			board: '=',
+			inMenu: '=',
+			group: '='
+		},
+		controller: function (LabelCache, $scope) {
+			var ctrl = this;
+			
+			$scope.$watch('lvgLabelPicker.label', function () {
+				ctrl.model = null;
+				ctrl.listValues = null;
+				if (ctrl.label && ctrl.label.type === 'LIST') {
+					LabelCache.findLabelListValues(ctrl.label.id).then(function (res) {
+						ctrl.listValues = res;
+					});
+				}
+			});
+		}
+	});
+	
+})();
+
+
+
