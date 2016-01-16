5235680d98b34902df4400dfcde574c1c488110b
erost <erost@digitalfondue.ch>
2016-01-16 16:01:22 +0100

    new navbar

---
+++ src/main/webapp/app/components/navbar/dashboard/navbar-dashboard.js
@@ -0,0 +1,23 @@
+(function() {
+    var components = angular.module('lavagna.components');
+
+    components.component('lvgNavbarDashboard', {
+        templateUrl: 'app/components/navbar/dashboard/navbar-dashboard.html',
+        controller: function($window, User, Sidebar) {
+             var ctrl = this;
+
+             User.currentCachedUser().then(function (u) {
+                 ctrl.user = u;
+             });
+
+             ctrl.toggleSidebar = function() {
+                Sidebar.toggle();
+             }
+
+             ctrl.login = function () {
+                 var reqUrlWithoutContextPath = $window.location.pathname.substr($("base").attr('href').length - 1);
+                 $window.location.href = 'login?reqUrl=' + encodeURIComponent(reqUrlWithoutContextPath);
+             }
+        }
+    });
+})();
