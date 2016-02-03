a8352f3595a95fdeef189231a1d276c8c6e2a641
erost <erost@digitalfondue.ch>
2016-02-03 21:07:49 +0100

    project settings navbar

---
+++ src/main/webapp/app/components/navbar/project-manage/navbar-project-manage.js
@@ -0,0 +1,33 @@
+(function() {
+    var components = angular.module('lavagna.components');
+
+    components.component('lvgNavbarProjectManage', {
+        templateUrl: 'app/components/navbar/project-manage/navbar-project-manage.html',
+        bindings: {
+            project: '='
+        },
+        controller: function($window, User, Sidebar, $rootScope, $state) {
+             var ctrl = this;
+
+             User.currentCachedUser().then(function (u) {
+                 ctrl.navbarUser = u;
+             });
+
+             ctrl.toggleSidebar = function() {
+                Sidebar.toggle();
+             }
+
+             ctrl.login = function () {
+                 var reqUrlWithoutContextPath = $window.location.pathname.substr($("base").attr('href').length - 1);
+                 $window.location.href = 'login?reqUrl=' + encodeURIComponent(reqUrlWithoutContextPath);
+             }
+
+             ctrl.navigationState = $state.current.name;
+             $rootScope.$on('$stateChangeSuccess',
+                function (event, toState, toParams, fromState, fromParams) {
+                    ctrl.navigationState = $state.current.name;
+                }
+             );
+        }
+    });
+})();
--- src/main/webapp/app/directives/lvg-navigation.js
+++ src/main/webapp/app/directives/lvg-navigation.js
@@ -4,29 +4,12 @@
 
 	var directives = angular.module('lavagna.directives');
 
-	directives.directive('lvgProjectNavigation', function () {
-		return {
-			restrict: 'E',
-			template: '<a data-ui-sref="home"><span data-translate>partials.home.title</span></a><i class="fa fa-angle-right"></i>' +
-			'{{::projectResolver.project.shortName}}<span class="separator">-</span>{{projectResolver.project.name}}<a data-lvg-has-permission="PROJECT_ADMINISTRATION" class="project-settings-shortcut" data-ui-sref="ProjectManage.project({projectName: projectResolver.project.shortName})" title="{{\'partials.project.fragments.nabvar.projectAdmin\' | translate}}"><i class="fa fa-cog"></i></a>'
-		}
-	});
-
 	directives.directive('lvgBoardNavigation', function () {
 		return {
 			restrict: 'E',
 			template: '<a data-ui-sref="home"><span data-translate>partials.home.title</span></a><i class="fa fa-angle-right"></i>' +
-			'<a data-ui-sref="project.boards({projectName: boardCtrl.project.shortName})">{{::boardCtrl.project.shortName}}</a> <a data-lvg-has-permission="PROJECT_ADMINISTRATION" class="project-settings-shortcut" data-ui-sref="ProjectManage.project({projectName: boardCtrl.project.shortName})" title="{{\'partials.project.fragments.nabvar.projectAdmin\' | translate}}"><i class="fa fa-cog"></i></a><i class="fa fa-angle-right"></i>' +
+			'<a data-ui-sref="project.boards({projectName: boardCtrl.project.shortName})">{{::boardCtrl.project.shortName}}</a> <a data-lvg-has-permission="PROJECT_ADMINISTRATION" class="project-settings-shortcut" data-ui-sref="projectManage.project({projectName: boardCtrl.project.shortName})" title="{{\'partials.project.fragments.nabvar.projectAdmin\' | translate}}"><i class="fa fa-cog"></i></a><i class="fa fa-angle-right"></i>' +
 			'{{::boardCtrl.board.shortName}}<span class="separator">-</span>{{boardCtrl.board.name}}'
-		}
-	});
-
-	directives.directive('lvgProjectSettingsNavigation', function () {
-		return {
-			restrict: 'E',
-			template: '<a data-ui-sref="home"><span data-translate>partials.home.title</span></a><i class="fa fa-angle-right"></i>' +
-			'<a data-ui-sref="project.boards({projectName: projectResolver.project.shortName})">{{::projectResolver.project.shortName}}</a><i class="fa fa-angle-right"></i>' +
-			'<span data-translate>partials.project.fragments.nabvar.admin</span>'
 		}
 	});
 
