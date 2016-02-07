94b02aebbc99c132710cbb33f31a07260d47c8b6
Simone Grignola <sito@grignola.ch>
2016-02-07 22:56:42 +0100

    ISSUTRAC-96 Unable to close a card's menu by clicking on the backdrop

--- src/main/webapp/app/directives/lvg-board-card-menu.js
+++ src/main/webapp/app/directives/lvg-board-card-menu.js
@@ -9,31 +9,31 @@
 		return {
 			restrict: 'A',
 			link: function($scope, element, attrs) {
-				
+
 				$scope.openCardMenu = function(card, columns) {
-					
+
 					//we create a specific scope for the card menu, as the current $scope can be destroyed at any time
 					var $scopeForCardMenu = $scope.$new();
-					
+
 					var cleanup = function () {
 						$('#cardModal,#cardModalBackdrop').removeClass('in');
 						$('#cardModal,#cardModalBackdrop').remove();
 						$("#cardBoardMenu").remove();
 						$(document).unbind('keyup', escapeHandler);
-						
+
 						$scopeForCardMenu.$destroy();
 					};
-					
+
 					var escapeHandler = function (e) {
 						if (e.keyCode == 27) {
 							cleanup();
 						}
 					};
-					
+
 					$scopeForCardMenu.close = function() {
 						cleanup();
-					}
-					
+					};
+
 					$scopeForCardMenu.move = function(card, toColumn) {
 						if(angular.isUndefined(toColumn)) {
 							return;
@@ -50,51 +50,52 @@
 							Board.moveCardToColumn(card.id, card.columnId, toColumn.id, {newContainer: ids});
 						}).then(cleanup);
 					};
-					
+
 					var top = $(element).offset().top;
 					var left = $(element).offset().left;
 					var size = $(element).width() + 2;
 					var height = $(element).height() + 2;
-					
+
 					var windowWidth = $(window).width();
 					var windowHeight = $(window).height();
-					
+
 					$scopeForCardMenu.moveColumns = $filter('filter')(columns, function(col) {return col.id != card.columnId});
-					
+
 					$(document).bind('keyup', escapeHandler);
-					
-					$("body").append($('<div id="cardModalBackdrop" class="lvg-modal-overlay lvg-modal-overlay-fade"></div>'));
-					$("#cardModal,#cardModalBackdrop").addClass('in');
-					
-					var template = $compile('<div id="cardBoardMenu" data-bindonce><lvg-card-fragment data-view="board" data-read-only data-card="card" data-board="board"></lvg-card-fragment>'
+
+					var template = $compile(
+                            '<div id="cardModalBackdrop" class="lvg-modal-overlay lvg-modal-overlay-fade" data-ng-click="close()"></div>'
+                            + '<div id="cardBoardMenu" data-bindonce><lvg-card-fragment data-view="board" data-read-only data-card="card" data-board="board"></lvg-card-fragment>'
 							+ '<div id="cardBoardMenuActions">'
 							+ '<div id="cardBoardMenuClose"><button type="button" class="close" data-ng-click="close()">&times;</button></div>'
 							+ '<div data-ng-include="\'partials/fragments/board-card-menu-card-actions-fragment.html\'"></div></div>'
 							+ '</div>')($scopeForCardMenu);
 					$("body").append(template);
-					
+
+                    $("#cardModal,#cardModalBackdrop").addClass('in');
+
 					//card overflows
 					if(windowHeight - top < height) {
 						top = windowHeight - (height + 10);
 					}
-					
+
 					$("#cardBoardMenu").css({
 						'position' : 'fixed',
 						'top' : top,
 						'left' : left,
 						'width' : size + 'px'
 					});
-					
+
 					//calculate actions position
 					var actionsCloseCSSObject = {};
 					var actionsCSSObject = {};
-					
+
 					if(windowHeight - top < 260) {
 						actionsCSSObject['bottom'] = 0;
 					} else {
 						actionsCSSObject['top'] = 0;
 					}
-					
+
 					if(left + size + 30 > windowWidth - size) {
 						actionsCSSObject['right'] = size + 10;
 						actionsCloseCSSObject['right'] = 0;
@@ -106,7 +107,7 @@
 					}
 					$("#cardBoardMenuActions").css(actionsCSSObject);
 					$("#cardBoardMenuClose").css(actionsCloseCSSObject);
-					
+
 				}
 			}
 		}
