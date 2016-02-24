35e5f1bba9c5b88fe01ba30bde3e2656cd4d4945
Sylvain Jermini <sylvain.jermini@syjer.com>
2016-02-24 19:43:51 +0100

    WORK-346 search only for label with LIST type

--- src/main/webapp/app/directives/lvg-search.js
+++ src/main/webapp/app/directives/lvg-search.js
@@ -143,7 +143,8 @@
                             var u = res.data[i];
                             var text = prefix + quoteIfHasSpace(u.name);
                             var type = "create";
-                            if (u.type !== 'NULL') {
+                            console.log(u.type);
+                            if (u.type === 'LIST') {
                                 text += ':';
                                 type = "example";
                             }
