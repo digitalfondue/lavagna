320639524f1184ac384aff21caf9fb63c6b5b6c3
Simone Grignola <sito@grignola.ch>
2016-03-04 07:35:54 +0100

    WORK-293 Add due dates to the milestones

--- src/main/webapp/app/directives/lvg-card-fragment.js
+++ src/main/webapp/app/directives/lvg-card-fragment.js
@@ -1,65 +1,85 @@
 (function () {
 
-	'use strict';
+    'use strict';
 
-	var directives = angular.module('lavagna.directives');
+    var directives = angular.module('lavagna.directives');
 
-	directives.directive('lvgCardFragment', function (Card) {
-		return {
-			templateUrl: 'partials/fragments/card-fragment.html',
-			restrict: 'E',
-			scope: {
-				card : '=card',
-				project : '=project',
-				board : '=board',
-				selected : '=selected',
-				query : '=query',
-				page : '=page',
-				dependencies : '=dependencies'
-			},
-			controller : function($scope, $element, $attrs) {
-				//controls attributes
-				$scope.readOnly = $attrs.readOnly != undefined;
-				$scope.hideAssignedUsers = $attrs.hideAssignedUsers != undefined;
-				
-				$scope.listView = $attrs.view != undefined && $attrs.view == 'list';
-				$scope.boardView = $attrs.view != undefined && $attrs.view == 'board';
-				$scope.searchView = $attrs.view != undefined && $attrs.view == 'search';
-				
-				//dependencies
-				angular.extend($scope, $scope.dependencies);
-				
-				$scope.hasUserLabels = function(cardLabels) {
-					if (cardLabels === undefined || cardLabels.length === 0) {
-						return false; //empty, no labels at all
-					}
-					for(var i = 0; i < cardLabels.length; i++) {
-						if(cardLabels[i].labelDomain === 'USER') {
-							return true;
-						}
-					}
-					return false;
-				};
+    directives.directive('lvgCardFragment', function (Card, LabelCache) {
+        return {
+            templateUrl: 'partials/fragments/card-fragment.html',
+            restrict: 'E',
+            scope: {
+                card: '=card',
+                project: '=project',
+                board: '=board',
+                selected: '=selected',
+                query: '=query',
+                page: '=page',
+                dependencies: '=dependencies'
+            },
+            controller: function ($scope, $element, $attrs) {
+                //controls attributes
+                $scope.readOnly = $attrs.readOnly != undefined;
+                $scope.hideAssignedUsers = $attrs.hideAssignedUsers != undefined;
 
-				$scope.hasSystemLabelByName = function(labelName, cardLabels) {
-					if (cardLabels === undefined || cardLabels.length === 0)
-						return false; //empty, no labels at all
-					for(var i = 0; i < cardLabels.length; i++) {
-						if(cardLabels[i].labelName == labelName && cardLabels[i].labelDomain === 'SYSTEM') {
-							return true;
-						}
-					}
-					return false;
-				};
-			
-				$scope.isSelfWatching = function(cardLabels, userId) {
-					return Card.isWatchedByUser(cardLabels, userId)
-				};
+                $scope.listView = $attrs.view != undefined && $attrs.view == 'list';
+                $scope.boardView = $attrs.view != undefined && $attrs.view == 'board';
+                $scope.searchView = $attrs.view != undefined && $attrs.view == 'search';
 
-				$scope.isAssignedToCard = function(cardLabels, userId) {
-					return Card.isAssignedToUser(cardLabels, userId);
-				};
-			}
-		}
-	});
+                //dependencies
+                angular.extend($scope, $scope.dependencies);
+
+                $scope.hasUserLabels = function (cardLabels) {
+                    if (cardLabels === undefined || cardLabels.length === 0) {
+                        return false; //empty, no labels at all
+                    }
+                    for (var i = 0; i < cardLabels.length; i++) {
+                        if (cardLabels[i].labelDomain === 'USER') {
+                            return true;
+                        }
+                    }
+                    return false;
+                };
+
+                $scope.hasSystemLabelByName = function (labelName, cardLabels) {
+                    if (cardLabels === undefined || cardLabels.length === 0)
+                        return false; //empty, no labels at all
+                    for (var i = 0; i < cardLabels.length; i++) {
+                        if (cardLabels[i].labelName == labelName && cardLabels[i].labelDomain === 'SYSTEM') {
+                            return true;
+                        }
+                    }
+                    return false;
+                };
+
+                var getMilestoneDate = function (label) {
+                    LabelCache.findLabelListValue(label.labelId, label.labelValueList).then(function (value) {
+                        label.releaseDate = value.metadata.releaseDate ? moment.utc(value.metadata.releaseDate, 'DD.MM.YYYY').valueOf() : null;
+                    });
+                };
+
+                var getMilestoneDates = function (card) {
+                    for (var i = 0; i < card.labels.length; i++) {
+                        var label = card.labels[i];
+                        if (label.labelName == 'MILESTONE' && label.labelDomain === 'SYSTEM') {
+                            getMilestoneDate(label);
+                        }
+                    }
+                };
+
+                $scope.$watch('card', function (val) {
+                    getMilestoneDates(val);
+                } );
+
+
+                $scope.isSelfWatching = function (cardLabels, userId) {
+                    return Card.isWatchedByUser(cardLabels, userId)
+                };
+
+                $scope.isAssignedToCard = function (cardLabels, userId) {
+                    return Card.isAssignedToUser(cardLabels, userId);
+                };
+            }
+        }
+    });
 })();
