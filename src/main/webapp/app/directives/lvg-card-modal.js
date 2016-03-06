0abf1efd284d2e8216fedbc29de23ee83df98360
Sylvain Jermini <sylvain.jermini@syjer.com>
2016-03-06 22:58:58 +0100

    WORK-345 transform lvg-card-modal in a component, fix various UX issues

--- src/main/webapp/app/directives/lvg-card-modal.js
+++ src/main/webapp/app/directives/lvg-card-modal.js
@@ -4,52 +4,53 @@
 
 	var directives = angular.module('lavagna.directives');
 
-	directives.directive('lvgCardModal', function ($mdDialog, $state) {
-		return {
-			restrict: 'E',
-            scope: {
-                project: '=',
-                board: '=',
-                card: '=',
-                user: '='
-            },
-			link: function ($scope, $element, $attrs) {
-                var goBack = function() {
-                    $state.go('^');
-                };
+	directives.component('lvgCardModal', {
+		bindings: {
+			project: '=',
+			board: '=',
+			card: '=',
+			user: '='
+		}, 
+		
+		controller: function($mdDialog, $state, $scope) {
+			
+			var ctrl = this;
+			
+            var goBack = function() {
+                $state.go('^');
+            };
 
-                $mdDialog.show({
-                    controller: DialogController,
-                    template: ['<md-dialog><md-dialog-content>',
-                               '<lvg-component-card card="modalCtrl.card" board="modalCtrl.board"',
-                               'project="modalCtrl.project" user="modalCtrl.user"',
-                               'close="modalCtrl.close"></lvg-component-card>',
-                               '</md-dialog-content></md-dialog>'].join(' '),
-                    parent: angular.element(document.querySelector('#cardModalAnchor')),
-                    clickOutsideToClose: true,
-                    fullscreen: true,
-                    locals: {
-                        project: $scope.project,
-                        board: $scope.board,
-                        card: $scope.card,
-                        user: $scope.user
-                    },
-                    bindToController: true,
-                    controllerAs: 'modalCtrl',
-                    onRemoving: goBack
-                });
+            $mdDialog.show({
+                controller: DialogController,
+                template: ['<md-dialog><md-dialog-content>',
+                           '<lvg-component-card card="modalCtrl.card" board="modalCtrl.board"',
+                           'project="modalCtrl.project" user="modalCtrl.user"',
+                           'close="modalCtrl.close"></lvg-component-card>',
+                           '</md-dialog-content></md-dialog>'].join(' '),
+                parent: angular.element(angular.element(document.body)),
+                clickOutsideToClose: true,
+                fullscreen: true,
+                locals: {
+                    project: ctrl.project,
+                    board: ctrl.board,
+                    card: ctrl.card,
+                    user: ctrl.user
+                },
+                bindToController: true,
+                controllerAs: 'modalCtrl',
+                onRemoving: goBack
+            });
 
-                function DialogController($mdDialog) {
-                    this.close = function() {
-                        $mdDialog.cancel();
-                    };
-                }
-
-                $scope.$on('$destroy', function() {
+            function DialogController($mdDialog) {
+                this.close = function() {
                     $mdDialog.cancel();
-                });
-			}
-		};
+                };
+            }
+
+            $scope.$on('$destroy', function() {
+                $mdDialog.cancel();
+            });
+		}
 	});
 
 })();
