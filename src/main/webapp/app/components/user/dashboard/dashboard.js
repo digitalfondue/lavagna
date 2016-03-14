31141674f3e4ab4b9f46354c6a56792c520e4112
Simone Grignola <sito@grignola.ch>
2016-03-14 23:59:43 +0100

    ISSUTRAC-107 fixed performance issues on the user's activity page

--- src/main/webapp/app/components/user/dashboard/dashboard.js
+++ src/main/webapp/app/components/user/dashboard/dashboard.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
 
     'use strict';
 
@@ -72,35 +72,19 @@
         var loadUser = function (profile) {
             ctrl.profile = profile;
             ctrl.user = profile.user;
-
-            ctrl.hasMore = profile.latestActivityByPage.length > 20;
             ctrl.activeProjects = profile.activeProjects;
-
-            ctrl.latestActivity20 = profile.latestActivityByPage.slice(0, 20);
-
-            ctrl.eventsGroupedByDate = groupByDate(profile.lastWeekActivity);
         };
 
         ctrl.userProvider = ctrl.profile.user.provider;
         ctrl.userName = ctrl.profile.user.username;
 
-        ctrl.page = 0;
-
-        ctrl.loadFor = function (page) {
-            User.getUserProfile(ctrl.userProvider, ctrl.userName, page)
-                .then(function (profile) {
-                    return loadUser(profile);
-                })
-                .then(function (profile) {
-                    showCalHeatMap(profile)
-                }).then(function () {
-                    ctrl.page = page
-                });
-        };
-
         //init
         loadUser(ctrl.profile);
 
         showCalHeatMap(ctrl.profile.dailyActivity);
+
+        User.getUserActivity(ctrl.profile.user.provider, ctrl.profile.user.username).then(function (activities) {
+            ctrl.eventsGroupedByDate = groupByDate(activities);
+        });
     }
 })();
