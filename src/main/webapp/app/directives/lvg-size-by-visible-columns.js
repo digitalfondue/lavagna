ee0878a311861bcfcb0cba446b08592bbd2db739
Sylvain Jermini <sylvain.jermini@syjer.com>
2016-07-31 16:11:03 +0200

    WORK-345 simplify the board width handling as the create new column functionality is outside the board now

--- src/main/webapp/app/directives/lvg-size-by-visible-columns.js
+++ src/main/webapp/app/directives/lvg-size-by-visible-columns.js
@@ -4,24 +4,17 @@
 
 	var directives = angular.module('lavagna.directives');
 
-	directives.directive('lvgSizeByVisibleColumns', function (User, $stateParams) {
+	directives.directive('lvgSizeByVisibleColumns', function () {
 		return {
 			restrict: 'A',
 			link: function ($scope, element, attrs) {
-				//select all li that have no lavagna-hide class
-
 				$scope.$watch('boardCtrl.columns.length', function (length) {
 					if(length === undefined) {
 						return;
 					}
-					User.hasPermission("CREATE_COLUMN", $stateParams.projectName).then(function () {
-						// count add column
-						var width = 290+ 4* 2+ (length* 290+ 4* 2* length);
-						element.css('width', width + "px");
-					}).catch(function() {
-						var width = length * 290 + 4* 2* length;
-                        element.css('width', width + "px");
-					});
+					//290 = width of column + 4px left and right margin
+					var width = length * (290 + 4 * 2);
+                    element.css('width', width + "px");
 				});
 			}
 		};
