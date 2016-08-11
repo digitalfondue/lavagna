4e2b63754fac70386177c1d972522947cd57d309
Sylvain Jermini <sylvain.jermini@syjer.com>
2016-08-11 15:02:10 +0200

    WORK-345 refactor label-val-v2: use dom directly

--- src/main/webapp/app/components/label-val-v2/label-val-v2.js
+++ src/main/webapp/app/components/label-val-v2/label-val-v2.js
@@ -5,96 +5,114 @@
 	var components = angular.module('lavagna.components');
 
 	components.component('lvgLabelValV2', {
-		template: '<span ng-bind="::$ctrl.displayValue" lvg-if-instant="::($ctrl.type != \'CARD\' && $ctrl.type != \'USER\')"></span>' +
-					'<a ng-href="{{::$ctrl.cardLink}}" lvg-if-instant="::($ctrl.type == \'CARD\')" ng-bind="::$ctrl.displayValue"></a>' + 
-					'<a ng-href="{{::$ctrl.userLink}}" lvg-if-instant="::($ctrl.type == \'USER\')"></a>',
 		bindings: {
 			valueRef: '&',
 			projectMetadataRef:'&'
 		},
-		controller: function($filter, $element, $rootScope, $state, CardCache, UserCache) {
-			var ctrl = this;
-			ctrl.value = ctrl.valueRef();
-			
-			var metadata = ctrl.projectMetadataRef();
-			
-			var type = ctrl.value.labelValueType || ctrl.value.type || ctrl.value.labelType;
-			var value = ctrl.value.value || ctrl.value;
-			
-			ctrl.type = type;
-			
+		controller: ['$filter', '$element', '$rootScope', '$state', '$window', 'CardCache', 'UserCache', lvgLabelValV2Ctrl]
+	});
+	
+	function lvgLabelValV2Ctrl($filter, $element, $rootScope, $state, $window, CardCache, UserCache) {
+		const ctrl = this;
+		const ctrl_value = ctrl.valueRef();
+		
+		const metadata = ctrl.projectMetadataRef();
+		
+		const type = ctrl_value.labelValueType || ctrl_value.type || ctrl_value.labelType;
+		const value = ctrl_value.value || ctrl_value;
+		
+		ctrl.$postLink = function lvgLabelValV2PostLink() {
 			if (type === 'STRING') {
-				ctrl.displayValue = value.valueString;
+				appendValueToSpan(value.valueString);
 			} else if (type === 'INT') {
-				ctrl.displayValue = value.valueInt;
+				appendValueToSpan(value.valueInt);
 			} else if (type === 'USER') {
 				handleUser(value.valueUser);
 			} else if (type === 'CARD') {
 				handleCard(value.valueCard);
 			} else if (type === 'LIST' && metadata && metadata.labelListValues && metadata.labelListValues[value.valueList]) {
-				ctrl.displayValue = metadata.labelListValues[value.valueList].value;
+				appendValueToSpan(metadata.labelListValues[value.valueList].value);
 			} else if (type === 'TIMESTAMP') {
-				ctrl.displayValue = $filter('date')(value.valueTimestamp, 'dd.MM.yyyy');
-			}
-			
-			//-------------
-			
-			function handleUser(userId) {
-				UserCache.user(userId).then(function (user) {
-					ctrl.userLink = $state.href('user.dashboard', {provider: user.provider, username: user.username});
-					var element = $element.find("a");
-					updateUser(user, element);
-					
-					var toDismiss = $rootScope.$on('refreshUserCache-' + userId, function () {
-						UserCache.user(userId).then(function(user) {
-							updateUser(user, element);
-						})
-					});
-					
-					ctrl.$onDestroy = function onDestroy() {
-						toDismiss();
-					};
-				});
-			}
-			
-			function updateUser(user, element) {
-				//CHECK
-				element.text($filter('formatUser')(user));
-				if (user.enabled) {
-					element.removeClass('user-disabled');
-				} else {
-					element.addClass('user-disabled');
-				}
-			}
-			//-------------
-			
-			function handleCard(cardId) {
-				CardCache.card(cardId).then(function (card) {
-					var element = $element.find("a");
-					ctrl.displayValue = card.boardShortName + '-' + card.sequence;
-					ctrl.cardLink = $state.href('board.card', {projectName: card.projectShortName, shortName: card.boardShortName, seqNr: card.sequence});
-					
-					updateCardClass(card, element);
-					
-					var toDismiss = $rootScope.$on('refreshCardCache-' + cardId, function () {
-						CardCache.card(cardId).then(function (card) {
-							updateCardClass(card, element);
-						});
-					});
-					
-					ctrl.$onDestroy = function onDestroy() {
-						toDismiss();
-					};
-				});
-			}
-			
-			function updateCardClass(card, element) {
-				if (card.columnDefinition != 'CLOSED') {
-					element.removeClass('lavagna-closed-card');
-				} else {
-					element.addClass('lavagna-closed-card');
-				}
+				appendValueToSpan($filter('date')(value.valueTimestamp, 'dd.MM.yyyy'));
 			}
 		}
-	});
+		
+		//-------------
+		
+		function appendValueToSpan(value) {
+			const span = $window.document.createElement('span');
+			span.textContent = value;
+			$element.append(span);
+		}
+		
+		function handleUser(userId) {
+			
+			const a = $window.document.createElement('a');
+			$element.append(a);
+			
+			UserCache.user(userId).then(function (user) {
+				const element = angular.element(a);
+				
+				element.attr('href', $state.href('user.dashboard', {provider: user.provider, username: user.username}));
+				
+				updateUser(user, element);
+				
+				const toDismiss = $rootScope.$on('refreshUserCache-' + userId, function () {
+					UserCache.user(userId).then(function(user) {
+						updateUser(user, element);
+					})
+				});
+				
+				ctrl.$onDestroy = function onDestroy() {
+					toDismiss();
+				};
+			});
+		}
+		
+		function updateUser(user, element) {
+			//CHECK
+			element.text($filter('formatUser')(user));
+			if (user.enabled) {
+				element.removeClass('user-disabled');
+			} else {
+				element.addClass('user-disabled');
+			}
+		}
+		//-------------
+		
+		function handleCard(cardId) {
+			
+			const a = $window.document.createElement('a');
+			$element.append(a);
+			
+			CardCache.card(cardId).then(function (card) {
+				const element = angular.element(a);
+				
+				a.textContent = card.boardShortName + '-' + card.sequence;
+				element.attr('href', $state.href('board.card', {projectName: card.projectShortName, shortName: card.boardShortName, seqNr: card.sequence}));
+				
+				updateCardClass(card, element);
+				
+				const toDismiss = $rootScope.$on('refreshCardCache-' + cardId, function () {
+					CardCache.card(cardId).then(function (card) {
+						updateCardClass(card, element);
+					});
+				});
+				
+				ctrl.$onDestroy = function onDestroy() {
+					toDismiss();
+				};
+			});
+		}
+		
+		function updateCardClass(card, element) {
+			if (card.columnDefinition != 'CLOSED') {
+				element.removeClass('lavagna-closed-card');
+			} else {
+				element.addClass('lavagna-closed-card');
+			}
+		}
+	}
+	
+	
 })();
