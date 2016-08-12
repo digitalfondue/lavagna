a7c59f47e3cc90152ec6dc2ac559aa486a8c2f63
Sylvain Jermini <sylvain.jermini@syjer.com>
2016-08-12 11:23:41 +0200

    WORK-345 refactor card-fragment-v2-head initial testing

--- src/main/webapp/app/components/card-fragment-v2/card-fragment-v2-head.js
+++ src/main/webapp/app/components/card-fragment-v2/card-fragment-v2-head.js
@@ -5,18 +5,37 @@
 		require : {
 			lvgCardFragmentV2 : '^lvgCardFragmentV2'
 		},
-		controller : [ '$element', lvgCardFragmentV2HeadCtrl ]
+		controller : [ '$element', '$compile', '$scope', lvgCardFragmentV2HeadCtrl ]
 	});
 
-	function lvgCardFragmentV2HeadCtrl($element) {
-		const
-		ctrl = this;
+	function lvgCardFragmentV2HeadCtrl($element, $compile, $scope) {
+		const ctrl = this;
 
-		ctrl.$onInit = function() {
+		ctrl.$postLink = function lvgCardFragmentV2HeadCtrlPostLink() {
+			$element.append($compile(template(ctrl.lvgCardFragmentV2))($scope));
 		}
-
-		ctrl.$postLink = function() {
+	}
+	
+	function template(ctrl) {
+		
+		var t = ''
+		
+		if(ctrl.boardView && !ctrl.readOnly) {
+			t += '<div>';
+			t += '<lvg-card-fragment-link dynamic-link="true" target-state="board.card" project-name="{{::$ctrl.lvgCardFragmentV2.projectShortName}}" board-short-name="{{::$ctrl.lvgCardFragmentV2.boardShortName}}" sequence-number="{{::$ctrl.lvgCardFragmentV2.card.sequence}}"></lvg-card-fragment-link>';
+			t += '</div>'
+			
+		} else if (ctrl.boardView && ctrl.readOnly) {
+			
+		} else if (ctrl.listView) {
+			
+		} else { //ctrl.searchView
+			
 		}
+		
+		t += '<span ng-bind="::$ctrl.lvgCardFragmentV2.card.name"></span>'
+		
+		return t;
 	}
 
 })();
