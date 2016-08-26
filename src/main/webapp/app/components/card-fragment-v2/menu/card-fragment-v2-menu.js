78ade15cba9545f1e69ab94b0d8bd6a290d89fd8
Sylvain Jermini <sylvain.jermini@syjer.com>
2016-08-26 17:04:55 +0200

    WORK-345 card-fragment-v2-menu : handle move to column

--- src/main/webapp/app/components/card-fragment-v2/menu/card-fragment-v2-menu.js
+++ src/main/webapp/app/components/card-fragment-v2/menu/card-fragment-v2-menu.js
@@ -2,7 +2,7 @@
 	'use strict';
 
 	angular.module('lavagna.components').component('lvgCardFragmentV2Menu', {
-		controller: ['BulkOperations', 'Card', lvgCardFragmentV2MenuCtrl],
+		controller: ['BulkOperations', 'Card', 'Board', '$filter', lvgCardFragmentV2MenuCtrl],
 		templateUrl: 'app/components/card-fragment-v2/menu/card-fragment-v2-menu.html',
 		bindings: {
 			mdPanelRef:'<',
@@ -14,8 +14,15 @@
 	});
 	
 	
-	function lvgCardFragmentV2MenuCtrl(BulkOperations, Card) {
+	function lvgCardFragmentV2MenuCtrl(BulkOperations, Card, Board, $filter) {
 		var ctrl = this;
+		
+		ctrl.$onInit = function lvgCardFragmentV2MenuCtrlOnInit() {
+			ctrl.moveColumnFlag = false;
+			ctrl.cloneCardFlag = false;
+			loadColumns();
+		}
+		
 		
 		ctrl.close = close;
 		ctrl.watchCard = watchCard;
@@ -23,6 +30,7 @@
 		ctrl.assignToCurrentUser = assignToCurrentUser;
 		ctrl.removeAssignForCurrentUser = removeAssignForCurrentUser;
 		ctrl.moveCard = moveCard;
+		ctrl.moveToColumn = moveToColumn;
 		
 		
 		var cardByProject = {};
@@ -48,7 +56,26 @@
 		
 		function moveCard(location) {
 			Card.moveAllFromColumnToLocation(ctrl.card.columnId, [ctrl.card.id], location);
-		};
+		}
+		
+		function loadColumns() {
+			Board.columnsByLocation(ctrl.card.boardShortName, 'BOARD').then(function(columns) {
+            	ctrl.moveColumns = $filter('filter')(columns, function(col) {return col.id != ctrl.card.columnId});
+            });
+		}
+		
+		function moveToColumn(toColumn) {
+			Card.findByColumn(toColumn.id).then(function(cards) {
+				var ids = [];
+				for (var i = 0;i<cards.length;i++) {
+					ids.push(cards[i].id);
+				}
+				ids.push(ctrl.card.id);
+				return ids;
+			}).then(function(ids) {
+				Board.moveCardToColumn(ctrl.card.id, ctrl.card.columnId, toColumn.id, {newContainer: ids});
+			}).then(close)
+		}
 		
 		
 		function close() {
