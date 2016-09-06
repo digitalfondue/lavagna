b42738b06bf60d17bfab1053c1e0e0f32ea3eff7
Sylvain Jermini <sylvain.jermini@syjer.com>
2016-09-06 19:47:32 +0200

    WORK-379 refactor: remove  from import controller

--- src/main/webapp/app/components/project/manage/import/import.js
+++ src/main/webapp/app/components/project/manage/import/import.js
@@ -6,10 +6,182 @@
         bindings: {
             project: '<'
         },
-        controller: ProjectManageImportController,
-        templateUrl: 'app/components/project/manage/import/import.html'
+        templateUrl: 'app/components/project/manage/import/import.html',
+        controller: ['$timeout', 'StompClient', 'Project', 'Admin', 'Notification', 'Board', ProjectManageImportController],
     });
 
+    function ProjectManageImportController($timeout, StompClient, Project, Admin, Notification, Board) {
+
+        var ctrl = this;
+        //
+        ctrl.connectToTrello = connectToTrello;
+        ctrl.checkShortName = checkShortName;
+        ctrl.suggestShortName = suggestShortName;
+        ctrl.manageImports = manageImports;
+        ctrl.cancel = cancel;
+        ctrl.importFromTrello = importFromTrello;
+        ctrl.loadTrello = loadTrello;
+        //
+        
+        var stompSub = angular.noop;
+        
+        ctrl.$onInit = function init() {
+        	ctrl.importSettings = {id: generateUUID(), archived: false};
+            ctrl.availableOrganizations = [];
+            ctrl.boardsToImport = 0;
+            ctrl.view = {};
+            
+            stompSub = StompClient.subscribe('/event/import/' + ctrl.importSettings.id, function (message) {
+                var body = JSON.parse(message.body);
+                ctrl.view.progress = 100.0 * body["currentBoard"] / body["boards"];
+                ctrl.view.currentBoardName = body["boardName"];
+            });
+            
+            Admin.findByKey('TRELLO_API_KEY').then(function (key) {
+                if (key.second == null) {
+                    ctrl.hasApiKey = false;
+                    return;
+                }
+
+                ctrl.hasApiKey = true;
+                ctrl.trelloApiKey = key.second;
+                ctrl.trelloSecret = '';
+                ctrl.trelloClientUrl = 'https://api.trello.com/1/client.js?key=' + ctrl.trelloApiKey;
+            });
+        };
+        
+        ctrl.$onDestroy = function onDestroy() {
+        	stompSub();
+        };
+        //
+        
+        
+
+        function checkShortName(board) {
+            if(board.shortName == null || board.shortName == "") {
+                board.checkedShortName = false;
+                return;
+            }
+            Board.checkShortName(board.shortName).then(function (res) {
+                board.checkedShortName = res;
+            });
+        }
+
+        function suggestShortName(board) {
+            if(board == null ||board.name == null || board.name == "") {
+                return;
+            }
+            Board.suggestShortName(board.name).then(function(res) {
+                board.shortName = res.suggestion;
+                ctrl.checkShortName(board);
+            });
+        }
+
+        function manageImports(board) {
+            ctrl.boardsToImport += board.import ? 1 : -1;
+            ctrl.suggestShortName(board);
+        }
+
+        function cancel() {
+            ctrl.availableOrganizations = [];
+            ctrl.view.progress = 0;
+            ctrl.view.currentBoardName = undefined;
+        }
+        
+        function importFromTrello() {
+            ctrl.view.trelloImportIsRunning = true;
+            ctrl.view.progress = 0;
+            ctrl.view.currentBoardName = undefined;
+            var boardsToImport = [];
+
+            angular.forEach(ctrl.availableOrganizations, function (o) {
+                angular.forEach(o.boards, function (b) {
+                    if (b.import) {
+                        boardsToImport.push({id: b.id, shortName: b.shortName});
+                        b.import = false;
+                        b.shortName = undefined;
+                        b.checkedShortName = undefined;
+                    }
+                });
+            });
+
+            Project.importFromTrello({
+                apiKey: ctrl.trelloApiKey,
+                secret: ctrl.trelloSecret,
+                projectShortName: ctrl.project.shortName,
+                importId: ctrl.importSettings.id,
+                importArchived: ctrl.importSettings.archived,
+                boards: boardsToImport
+            }).then(function () {
+                ctrl.view.trelloImportIsRunning = false;
+                Notification.addAutoAckNotification('success', {key: 'notification.project-manage-import.importFromTrello.success'}, false);
+            });
+        };
+        
+        
+        function loadTrello() {
+        	if(!window.Trello) {
+        		requireScript('/js/jquery-3.1.0.slim.min.js', function() {
+        			requireScript(ctrl.trelloClientUrl, function() {
+        				$timeout(function() {
+	            			ctrl.trelloLoaded = true;
+	            		})
+	            	});
+        		})
+        	} else {
+        		ctrl.trelloLoaded = true;
+        	}
+        }
+        
+        
+        
+        function connectToTrello() {
+        	//
+            Trello.authorize({
+                type: "popup",
+                name: "Lavagna",
+                expiration: "1day",
+                persist: false,
+                scope: {read: true, write: false, account: true},
+                success: function () {
+                	$timeout(function() {
+                    	ctrl.view.connectingToTrello = true;
+                        ctrl.trelloSecret = Trello.token();
+                        Project.getAvailableTrelloBoards({
+                            apiKey: ctrl.trelloApiKey,
+                            secret: ctrl.trelloSecret
+                        }).then(function (result) {
+                            ctrl.view.connectingToTrello = false;
+                            ctrl.availableOrganizations = result.organizations;
+                        });
+                	});
+                },
+                error: function () {
+                	$timeout(function() {
+                		ctrl.view.connectingToTrello = false;
+                		Notification.addAutoAckNotification('error', {key: 'notification.project-manage-import.importFromTrello.error'}, false);
+                	});
+                }
+            });
+        }
+    }
+    
+    // http://stackoverflow.com/a/14174028
+    function requireScript(url, callback){
+        var script = document.createElement('script');
+        script.src = url;
+        script.type = 'text/javascript';
+        script.onload = callback;
+        //Internet explorer
+        script.onreadystatechange = function() {
+            if (this.readyState == 'complete') {
+                callback();
+            }
+        }
+        document.getElementsByTagName("head")[0].appendChild(script);
+    }
+    
+    
     function generateUUID(){
         var d = new Date().getTime();
         var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
@@ -18,165 +190,6 @@
             return (c=='x' ? r : (r&0x3|0x8)).toString(16);
         });
         return uuid;
-    };
-
-    function ProjectManageImportController($scope, StompClient, Project, Admin, Notification, Board) {
-
-        var ctrl = this;
-        ctrl.view = {};
-
-        ctrl.importSettings = {id: generateUUID(), archived: false};
-        ctrl.availableOrganizations = [];
-        ctrl.boardsToImport = 0;
-
-        StompClient.subscribe('/event/import/' + ctrl.importSettings.id, function (message) {
-            var body = JSON.parse(message.body);
-            ctrl.view.progress = 100.0 * body["currentBoard"] / body["boards"];
-            ctrl.view.currentBoardName = body["boardName"];
-        }, $scope);
-
-        ctrl.checkShortName = function (board) {
-            if(board.shortName == null || board.shortName == "") {
-                board.checkedShortName = false;
-                return;
-            }
-            Board.checkShortName(board.shortName).then(function (res) {
-                board.checkedShortName = res;
-            });
-        };
-
-        ctrl.suggestShortName = function(board) {
-            if(board == null ||board.name == null || board.name == "") {
-                return;
-            }
-            Board.suggestShortName(board.name).then(function(res) {
-                board.shortName = res.suggestion;
-                ctrl.checkShortName(board);
-            });
-        };
-
-        ctrl.manageImports = function(board) {
-            ctrl.boardsToImport += board.import ? 1 : -1;
-            ctrl.suggestShortName(board);
-        }
-
-        ctrl.cancel = function () {
-            ctrl.availableOrganizations = [];
-            ctrl.view.progress = 0;
-            ctrl.view.currentBoardName = undefined;
-        };
-
-
-        Admin.findByKey('TRELLO_API_KEY').then(function (key) {
-            if (key.second == null) {
-                ctrl.hasApiKey = false;
-                return;
-            }
-
-            ctrl.hasApiKey = true;
-
-            var trelloApiKey = key.second;
-            var trelloSecret = '';
-            ctrl.trelloClientUrl = 'https://api.trello.com/1/client.js?key=' + trelloApiKey;
-
-            ctrl.importFromTrello = function () {
-                ctrl.view.trelloImportIsRunning = true;
-                ctrl.view.progress = 0;
-                ctrl.view.currentBoardName = undefined;
-                var boardsToImport = [];
-
-                angular.forEach(ctrl.availableOrganizations, function (o) {
-                    angular.forEach(o.boards, function (b) {
-                        if (b.import) {
-                            boardsToImport.push({id: b.id, shortName: b.shortName});
-                            b.import = false;
-                            b.shortName = undefined;
-                            b.checkedShortName = undefined;
-                        }
-                    });
-                });
-
-                Project.importFromTrello({
-                    apiKey: trelloApiKey,
-                    secret: trelloSecret,
-                    projectShortName: ctrl.project.shortName,
-                    importId: ctrl.importSettings.id,
-                    importArchived: ctrl.importSettings.archived,
-                    boards: boardsToImport
-                }).then(function () {
-                    ctrl.view.trelloImportIsRunning = false;
-                    Notification.addAutoAckNotification('success', {key: 'notification.project-manage-import.importFromTrello.success'}, false);
-                });
-            };
-            
-            //
-            // 
-            // http://stackoverflow.com/a/14174028
-            function requireScript(url, callback){
-                var head = document.getElementsByTagName("head")[0];
-                var script = document.createElement('script');
-                script.src = url;
-                script.type = 'text/javascript';
-                script.onload = callback;
-                //Internet explorer
-                script.onreadystatechange = function() {
-                    if (this.readyState == 'complete') {
-                        callback();
-                    }
-                }
-                head.appendChild(script);
-            }
-            
-            //
-            
-            ctrl.loadTrello = function() {
-            	if(!window.Trello) {
-            		requireScript('/js/jquery-3.1.0.slim.min.js', function() {
-            			requireScript(ctrl.trelloClientUrl, function() {
-    	            		$scope.$applyAsync(function() {
-    	            			ctrl.trelloLoaded = true;
-    	            		})
-    	            	});
-            		})
-            	} else {
-            		ctrl.trelloLoaded = true;
-            	}
-            }
-            
-            ctrl.connectToTrello = function() {
-            	authorizeToTrello();
-            }
-
-            function authorizeToTrello() {
-            	//
-                Trello.authorize({
-                    type: "popup",
-                    name: "Lavagna",
-                    expiration: "1day",
-                    persist: false,
-                    scope: {read: true, write: false, account: true},
-                    success: function () {
-                    	$scope.$applyAsync(function() {
-	                    	ctrl.view.connectingToTrello = true;
-	                        trelloSecret = Trello.token();
-	                        Project.getAvailableTrelloBoards({
-	                            apiKey: trelloApiKey,
-	                            secret: trelloSecret
-	                        }).then(function (result) {
-	                            ctrl.view.connectingToTrello = false;
-	                            ctrl.availableOrganizations = result.organizations;
-	                        });
-                    	});
-                    },
-                    error: function () {
-                    	$scope.$applyAsync(function() {
-                    		ctrl.view.connectingToTrello = false;
-                    		Notification.addAutoAckNotification('error', {key: 'notification.project-manage-import.importFromTrello.error'}, false);
-                    	});
-                    }
-                });
-            }
-        });
-
-    };
+    }
+    
 })();
