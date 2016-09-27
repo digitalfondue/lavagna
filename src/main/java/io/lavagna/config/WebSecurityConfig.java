8bd02807d2220867601f8fa16ab1c15e29d7da21
Sylvain Jermini <sylvain.jermini@syjer.com>
2016-09-27 13:48:40 +0200

    REL11-50 use md-checkbox in login page

--- src/main/java/io/lavagna/config/WebSecurityConfig.java
+++ src/main/java/io/lavagna/config/WebSecurityConfig.java
@@ -69,6 +69,7 @@ public class WebSecurityConfig {
                 .sessionHandler(sessionHandler)
                 .request("/favicon.ico").permitAll()
                 .request("/css/**").permitAll()
+                .request("/fonts/**").permitAll()
                 .request("/resource-login/**").permitAll()
                 .request("/setup/**").denyAll()
                 .request("/api/calendar/**").permitAll()
