b250a015195713eb9f5063a62d27c7e4db285140
Sylvain Jermini <sylvain.jermini@syjer.com>
2016-11-11 16:07:34 +0100

    fix nullity check

--- src/main/kotlin/io/lavagna/model/ProjectWithEventCounts.kt
+++ src/main/kotlin/io/lavagna/model/ProjectWithEventCounts.kt
@@ -21,7 +21,7 @@ import ch.digitalfondue.npjt.ConstructorAnnotationRowMapper.Column
 class ProjectWithEventCounts(@Column("PROJECT_ID") projectId: Int,
                              @Column("PROJECT_NAME") projectName: String,
                              @Column("PROJECT_SHORT_NAME") projectShortName: String,
-                             @Column("PROJECT_DESCRIPTION") projectDescription: String,
+                             @Column("PROJECT_DESCRIPTION") projectDescription: String?,
                              @Column("PROJECT_ARCHIVED") projectArchived: Boolean, //
                              @Column("EVENTS") val events: Long) {
     val project: Project
