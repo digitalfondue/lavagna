d7e3e91ef2496c30fbd7b221e33d3bee356a6f31
Sylvain Jermini <sylvain.jermini@syjer.com>
2017-02-21 15:39:23 +0100

    REL11-14 stub for add new integration dialog

--- src/main/webapp/app/components/admin/integrations/integrations.js
+++ src/main/webapp/app/components/admin/integrations/integrations.js
@@ -5,17 +5,29 @@
 
     components.component('lvgAdminIntegrations', {
     	templateUrl: 'app/components/admin/integrations/integrations.html',
-        controller: ['Integrations', AdminIntegrationsController]
+        controller: ['$mdDialog','Integrations', AdminIntegrationsController]
     });
 
 
-    function AdminIntegrationsController(Integrations) {
+    function AdminIntegrationsController($mdDialog, Integrations) {
 
         var ctrl = this;
+
+        ctrl.addNewIntegrationDialog = addNewIntegrationDialog;
 
         ctrl.$onInit = function() {
             Integrations.getAll().then(function(res) {
                 ctrl.integrations = res;
+            });
+        };
+
+        function addNewIntegrationDialog() {
+            $mdDialog.show({
+                templateUrl: 'app/components/admin/integrations/add-new-integration-dialog.html',
+                controller: function() {
+                },
+                controllerAs: 'addNewIntegrationCtrl',
+                bindToController: true
             });
         }
     }
