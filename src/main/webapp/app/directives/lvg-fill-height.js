3fe1ea23d106f430314746a82b1748e01f7dc9ea
Eros Taborelli <erost@erost.net>
2017-04-02 16:45:40 +0200

    REL11-85: fix empty column resize when filtering

--- src/main/webapp/app/directives/lvg-fill-height.js
+++ src/main/webapp/app/directives/lvg-fill-height.js
@@ -3,55 +3,6 @@
 
     var directives = angular.module('lavagna.directives');
 
-    /*
-     *  we have the following situation:
-     *
-     *  +----------------------+     +
-     *  |  top                 |    |
-     *  +--------------------- +-------------------------- $(element).position().top
-     *  |                           +--- window.height()
-     *  |  canvas area              |
-     *  |                           |
-     *  |                           |
-     *  +----------------------     +
-     *
-     *
-     *  The canvas area must have an height that fill the current view port if we
-     *  want that the horizontal scroll bar to appear at the bottom of the page.
-     *
-     *  Additionally, we support the presence of a left sidebar with an absolute
-     *  position.
-     *
-     *
-     *  Thus:
-     */
-    directives.directive('lvgUpdateColumnSize', function ($window) {
-        return {
-            restrict: 'A',
-            link: function lvgUpdateColumnSizeLink($scope, element) {
-                var domElement = element[0];
-
-                var resizeHandler = function () {
-                    var wHeight = $window.innerHeight;//
-                    var maxHeight = wHeight - 96; // fixed header + board-controls
-
-                    var head = domElement.querySelector('.lvg-board-column__header');
-                    var panelHeadHeight = head ? head.offsetHeight : 0;
-
-                    domElement.querySelector('.lvg-board-column__content').style.maxHeight = (maxHeight - panelHeadHeight) + 'px';
-                };
-
-                resizeHandler();
-
-                $window.addEventListener('resize', resizeHandler);
-
-                $scope.$on('$destroy', function () {
-                    $window.removeEventListener('resize', resizeHandler);
-                });
-            }
-        };
-    });
-
     directives.directive('lvgUpdateSidebarSize', function ($window) {
         return {
             restrict: 'A',
@@ -60,7 +11,7 @@
 
                 var resizeHandler = function lvgFillSidebarCardsHeightResizeHandler(hasMore) {
                     var wHeight = $window.innerHeight;//
-                    var maxHeight = wHeight - 96; // fixed header + board-controls
+                    var maxHeight = wHeight - 72; // fixed header + margins
 
                     var head = domElement.querySelector('.lvg-board-sidebar__header');
                     var panelHeadHeight = head ? head.offsetHeight : 0;
